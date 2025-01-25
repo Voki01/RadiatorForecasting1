54a3553c0d7b2b7c1b5b594492c3970b7142cc44
Dmitry M <diman-mik@mail.ru>
2025-01-25 15:27:02 +0500

    Панель навигации для руководителя

---
+++ RadiatorForecasting/Controllers/ManagerDashboardController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RadiatorForecasting.Controllers
+{
+    public class ManagerDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Отображение заглушки при первой загрузке
+            return View();
+        }
+
+        public IActionResult Main()
+        {
+            // В будущем тут будет функционал раздела "Главная"
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            // В будущем тут будет функционал раздела "Формирование отчета"
+            return View();
+        }
+
+        public IActionResult NetworkSettings()
+        {
+            // В будущем тут будет функционал раздела "Настройки сети"
+            return View();
+        }
+    }
+}
